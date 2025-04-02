625847c74ba51095160d918cb6670c3d5809eaff
Наталья Скворцова <fnatalya1999@mail.ru>
2025-04-02 21:55:28 +0300

    правки

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,7 @@ public class App {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+                (HelloWorld) applicationContext.getBean("Hello World");
         System.out.println(bean.getMessage());
     }
 }
